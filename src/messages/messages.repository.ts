2476dceaedfdb2235f0660cdf072d3815ac5cd1d
awesomeji <thswl1515@gmail.com>
2023-11-23 13:57:59 +0900

    message

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,6 +1,6 @@
 import { readFile, writeFile } from "fs/promises";
 
-export class MessageRepository {
+export class MessagesRepository {
     async findOne(id: string) {
         const contents = await readFile('messages.json', 'utf8');
         const messages = JSON.parse(contents);
